85a15119acb25ed0d787fc643698366457984980
Ruben Stolk <ruben.stolk@changer.nl>
2014-10-03 15:28:30 +0530

    Check for basic auth

--- server.js
+++ server.js
@@ -15,20 +15,19 @@ catch(e) {}
 
 /* This is a nasty hack to hijack into the express middlewares
    because atlasboard doesn't expose express */
-/*var express = require('./node_modules/atlasboard/node_modules/express');
-var methodOverride = express.methodOverride;
-express.__defineGetter__('methodOverride', function () {
-  return function (key) {
-    return function (req, res, next) {
-      if (globalAuth.basic) {
+if (globalAuth.basic && globalAuth.basic.username && globalAuth.basic.password) {
+  var express = require('./node_modules/atlasboard/node_modules/express');
+  var methodOverride = express.methodOverride;
+  express.__defineGetter__('methodOverride', function () {
+    return function (key) {
+      return function (req, res, next) {
         return express.basicAuth(globalAuth.basic.username, globalAuth.basic.password)(req, res, function () {
           return methodOverride(key)(req, res, next);
         });
-      }
-      return methodOverride(key)(req, res, next);
+      };
     };
-  };
-});*/
+  });
+}
 
 // Start atlasboard server
 var atlasboard = require('./node_modules/atlasboard/lib/atlasboard');
