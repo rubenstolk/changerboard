a7074837cc206a448d77e504085e9efc0bf5915d
Ruben Stolk <ruben.stolk@changer.nl>
2014-10-03 11:29:13 +0530

    Rename dashboard and get globalAuth from env

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 'use strict';
 
+var fs = require('fs');
+var globalAuth = {};
+try {
+  globalAuth = JSON.parse(process.env.GLOBAL_AUTH || '{}');
+  fs.writeFileSync('./globalAuth.json', JSON.stringify(globalAuth));
+}
+catch(e) {}
+
 var atlasboard = require('./node_modules/atlasboard/lib/atlasboard');
 atlasboard({ port: process.env.PORT || 5000 }, function (err) {
+  fs.unlink('./globalAuth.json');
   if (err) {
     console.log(err);
   }
